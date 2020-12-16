8018b3ff6e1ad3b351b4bada1f515c0dedd35c41
iscoups <2572209130@qq.com>
2020-12-16 09:43:05 +0800

    修改广播

--- src/components/broadcast/Broadcast.jsx
+++ src/components/broadcast/Broadcast.jsx
@@ -19,7 +19,7 @@ export default class Broadcast extends React.Component{
                     name:element.name,
                     supplier:element.supplier,
                     productBatch:element.productBatch,
-                    createdAt:element.createdAt,
+                    createdAt:element.createdAt.substring(0,10),
                     purchaseAt:element.purchaseAt,
                     ip:element.ip,
                     volume:element.volume,
@@ -111,21 +111,24 @@ export default class Broadcast extends React.Component{
             <h5>广播设备信息:</h5>
            
             {info.map((item,index)=>{
-               return   <ul>
-                            <li>设备编码：{item.code}</li>
-                            <li>设备型号:{item.model}</li>
-                            <li>设备名称:{item.name}</li>
-                            <li>供应商:{item.supplier}</li>
-                            <li>生产批次:{item.productBatch}</li>
-                            <li>安装时间:{item.createdAt}</li>
-                            <li>采购日期:{item.purchaseAt}</li>
-                            <li>IP地  址:{item.ip}</li>
-                            <li>音    量:{item.volume}</li>
-                            <li>运行状态:{item.on}</li>
+               return   <ul style={{fontSize:"16px"}}>
+                            <li key={index}>设备编码：{item.code}</li>
+                            <li key={index}>设备型号:{item.model}</li>
+                            <li key={index}>设备名称:{item.name}</li>
+                            <li key={index}>供应商:{item.supplier}</li>
+                            <li key={index}>生产批次:{item.productBatch}</li>
+                            <li key={index}>安装时间:{item.createdAt}</li>
+                            <li key={index}>采购日期:{item.purchaseAt}</li>
+                            <li key={index}>IP地  址:{item.ip}</li>
+                            <li key={index}>音    量:{item.volume}</li>
+                            <li key={index}>运行状态:{item.on===true ? "打开":"关闭"}</li>
                          </ul>
                         
                       
             })}
+
+            <h5>广播内容信息：</h5>
+            
         </div>
         )
     }
